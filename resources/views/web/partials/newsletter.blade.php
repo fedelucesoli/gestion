d0364828001b37fb015011f0a0eb8b36c2b4e1f7
fedelucesoli <fedelucesoli@gmail.com>
2017-07-16 20:47:43 -0300

    Newsletter basico

--- resources/views/web/partials/newsletter.blade.php
+++ resources/views/web/partials/newsletter.blade.php
@@ -1,16 +1,16 @@
 <!-- Begin MailChimp Signup Form -->
 <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-
-<div id="mc_embed_signup" class="newsletter">
-<form action="//gob.us16.list-manage.com/subscribe/post?u=ed7f1819568e62a49bd0b030e&amp;id=3728a59b97" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Suscribite al boletín de noticias del Municipio de Lobos</label>
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ed7f1819568e62a49bd0b030e_3728a59b97" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Suscribirse" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-
+<section class="box-linea-round container" style="margin-top: 50px">
+  <div id="mc_embed_signup" class="">
+  <form action="//gob.us16.list-manage.com/subscribe/post?u=ed7f1819568e62a49bd0b030e&amp;id=3728a59b97" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+    <label for="mce-EMAIL">Suscribite al boletín de noticias del Municipio de Lobos</label>
+    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email" required>
+      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ed7f1819568e62a49bd0b030e_3728a59b97" tabindex="-1" value=""></div>
+      <div class="clear"><input type="submit" value="Suscribirse" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+      </div>
+  </form>
+  </div>
+</section>
 <!--End mc_embed_signup-->
