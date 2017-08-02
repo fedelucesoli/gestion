b01492e7385f0e747513c86c63383e6045b163c6
FedeLucesoli <fede.lucesoli@gmail.com>
2017-08-02 13:56:15 -0300

    Miercoles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,20 +46,24 @@
            </ul>
        </li>
    @endif --}}
-     <div class="container box-linea">
-       <div class="row">
-         <div class="col-md-12">
+   <div class="container">
+     <div class="row">
+       <div class="col-md-12">
 
-             <ul class="nav">
-               <li class="{{ Request::is('admin') ? 'activo' : '' }}" ><a href="{{route('admin.dashboard')}}" >Escritorio</a></li>
-               <li class="{{ Request::is('admin/obras/*') || Request::is('admin/obras')  ? 'activo' : '' }}" ><a href="{{route('admin.obras.index')}}" >Obras</a></li>
-               <li class="{{ Request::is('admin/categoria') || Request::is('admin/categoria/*') ? 'activo' : '' }}"><a href="{{route('admin.categoria.index')}}">Categorias</a></li>
+           <ul class="nav">
+             <li class="{{ Request::is('admin') ? 'activo' : '' }}" ><a href="{{route('admin.dashboard')}}" >Escritorio</a></li>
+             <li class="{{ Request::is('admin/obras/*') || Request::is('admin/obras')  ? 'activo' : '' }}" ><a href="{{route('admin.obras.index')}}" >Obras</a></li>
+             <li class="{{ Request::is('admin/categoria') || Request::is('admin/categoria/*') ? 'activo' : '' }}"><a href="{{route('admin.categoria.index')}}">Categorias</a></li>
 
-               <li class="right"><a href="">{{ Auth::user()->name }} <span class="caret"></span></a></li>
-             </ul>
+             <li class="right"><a href="">{{ Auth::user()->name }} <span class="caret"></span></a></li>
+           </ul>
 
-         </div>
        </div>
+     </div>
+   </div>
+
+     <div class="container box-linea">
+
        @if (Session::has('mensaje'))
             <div class="alert alert-info">{{ Session::get('mensaje') }}</div>
         @endif
